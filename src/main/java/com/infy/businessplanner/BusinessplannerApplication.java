5b85c7d817819729eaf6df9cfc1b96077e903a02
Ryga171 <83324943+Ryga171@users.noreply.github.com>
2021-05-03 08:12:01 +0530

    Code commit

--- src/main/java/com/infy/businessplanner/BusinessplannerApplication.java
+++ src/main/java/com/infy/businessplanner/BusinessplannerApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BusinessplannerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Running application");
 		SpringApplication.run(BusinessplannerApplication.class, args);
+
+
 	}
 
 }
